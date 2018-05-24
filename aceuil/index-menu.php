0786a762466ea6ab1392b3aced99c3ea4f09a405
YadelAmendah <39162815+YadelAmendah@users.noreply.github.com>
2018-05-24 19:13:58 +0000

    Add files via upload

---
+++ aceuil/index-menu.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+ <html >
+ <head>
+     <meta charset="utf-8">
+     <title>ESGIS/GESTION DE NOTES</title>
+
+     <!--  font-->
+     <link rel="stylesheet" href="font/glyphicons-halflings-regular.ttf"> 
+
+     <!-- css -->
+     <script async="" src="style.js"> </script>
+     <link rel="shortcut icon" href="alienware-normal.ico">
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+     <link rel="stylesheet" href="style.css">
+ </head>
+ <!-- contenu du site -->
+ <body>
+    <!--banniere-->
+		 <?php include('ban.php'); ?>
+     <!--end banniere-->
+
+   <!-- debut de actu -->
+   <section class="container-fluid about">
+     <div class="container">
+       <div class="row">
+         <h2 id="ACTUALITE">Actualite</h2>
+         <hr class="separateur">
+         <article class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+           <h2>Mutuelle Santé Sans Frontière</h2> <br />
+           <p>Le droit à la santé fait partie des droits fondamentaux de l’Homme. Aussi tous les Etats ont-ils l’obligation de mettre en œuvre les moyens nécessaires pour garantir le droit à la santé de leurs populations. Il s’agit de leur favoriser un accès en temps utile, à des soins de santé acceptables d’une qualité satisfaisante et d’un coût abordable. Malheureusement, ce droit semble être hypothéqué dans bon nombre de pays en voie de développement
+          </p>
+         </article>
+         <article class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+           <h2>Au Togo</h2> <br />
+           <p>l’accessibilité financière aux soins de santé de la population pauvre et vulnérable se pose avec acuité.
+Au regard de ces constats, les étudiants en fin de semestre 5 et 6 en filière Management International (MI) à l’Ecole Supérieure de Gestion d’Informatique et des Sciences (ESGIS), avaient choisi et élaboré dans le cadre de leur formation un projet dénommé « Mutuelle Santé Sans Frontière » comme solution alternative en élargissant l’accès aux soins de santé aux populations pauvres et vulnérable.Outre ce premier objectif, la mise en place de cette mutuelle santé serait un canal de création d’emploi aussi bien aux diplômés de l’ESGIS et que pour d’autres jeunes.
+          </p>
+         </article>
+         <article class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+          <h2>Mutuelle Santé Sans Frontière</h2> <br />
+           <p>Le droit à la santé fait partie des droits fondamentaux de l’Homme. Aussi tous les Etats ont-ils l’obligation de mettre en œuvre les moyens nécessaires pour garantir le droit à la santé de leurs populations. Il s’agit de leur favoriser un accès en temps utile, à des soins de santé acceptables d’une qualité satisfaisante et d’un coût abordable. Malheureusement, ce droit semble être hypothéqué dans bon nombre de pays en voie de développement
+          </p>
+         </article>
+       </div>
+     </div>
+   </section><br />
+   <!-- debut de actu -->
+
+   <!--a propos-->
+   <section class="container-fluid about">
+     <div class="container">
+       <div class="row">
+         <h2 id="A-PROPOS">A propos</h2>
+         <hr class="separateur">
+         <article class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+           <h2>HTML</h2> <br/><br/>
+           <p>
+             Le langage HTML (HyperText Markup Language) est un langage permettant d'écrire des documents hypertexte pour le web. C'est un langage très simple, basé sur des balises (tag). Les balises servent à mettre en forme le texte, à réaliser des liens vers d'autres pages, à insérer des images, etc… Le langage HTML (HyperText Markup Language) est un langage permettant d'écrire des documents hypertexte pour le web. C'est un langage très simple, basé sur des balises (tag). Les balises servent à mettre en forme le texte, à réaliser des liens vers d'autres pages, à insérer des images, etc…
+           </p>
+         </article>
+         <article class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+           <h2>Architecture de von Neumann</h2>
+           <p>
+             Machine universelle programm´ee Instructions num´eriques stock´ees en m´emoire Instructions modiﬁables ex´ecut´ees en s´equence avec des ruptures de s´equence (sauts).
+unit´e arithm´etique et logique (UAL) qui eﬀectue les op´erations de base, unit´e de contrˆole qui est charg´ee du s´equen¸cage des op´erations, m´emoire qui contient `a la fois les donn´ees et le programme, dispositifs d’entr´ee-sortie qui permettent de communiquer avec le monde ext´erieur.
+Actuellement, la plupart des ordinateurs sont des machines de Von Neumann, seules les technologies ont chang´e.
+           </p>
+         </article>
+         <article class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+           <h2>Généralité sur les fonctions</h2>
+           <p>
+             Une loi f qui associe à chaque élément d’un ensemble D un unique élément d’un ensembleY est une fonction de D versY. On écrit y = f(x) pour signiﬁer que y est l’élément de Y qui est associé à l’élément x de D par f. L’ensemble D est l’ensemble de déﬁnition de f ; il est noté Df. Les éléments de Y sont les valeurs possibles de f. Si y0 ∈ Y et s’il existe x0 ∈ D telque f(x0) = y0,onditque f atteintlavaleur y0.L’ensembledesvaleursatteintes par f est l’image de f. Une fonction réelle de variable réelle est une fonction dont le domaine et l’image sont tous des parties de R.
+           </p>
+         </article>
+       </div>
+     </div>
+   </section>
+
+ </body>
+
+ </html>
